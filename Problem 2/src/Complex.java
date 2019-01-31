c6219848fec2a4a80215e1390567c5ab64849234
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2019-01-31 14:21:25 -0800

    Get'em

--- Problem 2/src/Complex.java
+++ Problem 2/src/Complex.java
@@ -1,27 +1,38 @@
 
 public class Complex {
+	
+	int re;
+	int im;
+	
+	Complex(int im, int re) {
+		
+	}
+	
+	Complex() {
+		
+	}
+	private class plus{
+		
+	} 
 
+	private class minus{
+		
+	}
+
+	private class times{
+		
+	}
+
+	private class divide{
+		
+	}
+
+	private class abs{
+		
+	}
+
+	private class toString{
+		
+	} 
+	
 }
-class plus{
-	
-} 
-
-class minus{
-	
-}
-
-class times{
-	
-}
-
-class divide{
-	
-}
-
-class abs{
-	
-}
-
-class toString{
-	
-} 
