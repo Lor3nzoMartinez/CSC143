eac1e1614d67ac9bb769f5ac4a39db138ddd5ff0
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2019-02-02 12:51:09 -0800

    the date that is up

--- L3_ArrayLab/src/algorithms/AlgorithmClient.java
+++ L3_ArrayLab/src/algorithms/AlgorithmClient.java
@@ -1,11 +1,19 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AlgorithmClient {
 	
-	static int[] list = {1,2,4,6,7,9};
-	static int[] list1 = {2,4,5,7,8,9};
+	static int[] list1 = {1,2,4,6,9};
+	static int[] list2 = {1,2,8,2,7,4,2,6,8,7,8,4,2,4,2,4,9};
+	static int[] list3 = {1,2,4,6,7,9};
+	static int[] list4 = {2,4,5,7,8,9};
+	static int[] list5 = {2,4,5,7,8,9};
+	static int[] list6 = {1,2,4,5,7,8,9};
+	
+	
 	
 	
 	public static void print (int[] list) {
@@ -32,26 +40,82 @@ public class AlgorithmClient {
 	}
 	
 	public static void shiftLeft(int[] list) {
-		for (int i = 0; i < list.length; ++i) {
-			if(i > 1) 
-				list[i-1] = list[i];
-			list[0] = list[list.length-1];
+		int last = list[0];
+		for (int i = 0; i < list.length-1; ++i) {
+			list[i] = list[i+1];
+			}
+		list[list.length-1] = last;
+		print(list);
+	}
+	
+	public static void helperShiftLeft(int[] list) {
+		int last = list[0];
+		for (int i = 0; i < list.length-1; ++i) {
+			list[i] = list[i+1];
+			}
+		list[list.length-1] = last;
+		//print(list);
+	}
+	
+	public static void shiftRight(int[] list) {
+		for (int i = 0; i < list.length-1; ++i) {
+			helperShiftLeft(list);
 		}
 		print(list);
 	}
 	
+	public static void helperShiftRight(int[] list) {
+		for (int i = 0; i < list.length-1; ++i) {
+			helperShiftLeft(list);
+		}
+		//print(list);
+	}
+	
+	public static void insert(int[] list) {
+		int [] saveList =new int[list.length+1];
+		for (int firstHalf = 0; firstHalf < list.length/2; ++firstHalf) {
+			saveList[firstHalf] = list[firstHalf];
+		}
+		for (int lastHalf = 4; lastHalf < list.length; ++lastHalf) {
+			saveList[lastHalf] = list[lastHalf-1];
+		}
+		print(saveList);
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		print(list);
-		System.out.println(getKeyCount(list,2));
-		System.out.println(getKeyCount(list,4));
-		System.out.println(getKeyCount(list,8));
-		System.out.println(indexOf(list,2));
-		System.out.println(indexOf(list,7));
-		System.out.println(indexOf(list,8));
+		
+		// Problem 1
 		print(list1);
-		shiftLeft(list1);
+		System.out.println("\n");
+		
+		//Problem 2
+		System.out.println(getKeyCount(list2,2));
+		System.out.println(getKeyCount(list2,4));
+		System.out.println(getKeyCount(list2,8));
+		System.out.println("\n");
+		
+		//Problem 3
+		System.out.println(indexOf(list3,2));
+		System.out.println(indexOf(list3,7));
+		System.out.println(indexOf(list3,8));
+		System.out.println("\n");
+		
+		//Problem 4
+		print(list4);
+		shiftLeft(list4);
+		System.out.println("\n");
+		
+		//Problem 5
+		print(list5);
+		shiftRight(list5);
+		System.out.println("\n");
+		
+		//Problem 6
+		print(list6);
+		insert(list6);
 	}
 
 }
